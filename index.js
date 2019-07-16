0ee11885cb95a45d5aef6a7b8c877bc26a34a2fc
Monique Hennington <mrs_mhen@hotmail.com>
2019-07-16 17:10:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ function whisper(string) {
 function logShout(string) {
   return console.log (string.toUpperCase())
 }
+
+function logWhisper(string) {
+  return console.log(string.toLowerCase())
+}
 function sayHiToGrandma(string) {
   return "I can't hear you!"
 }
